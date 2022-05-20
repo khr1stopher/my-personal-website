9a0b5b82e61b267ab759b423859d4a171a9d84a8
Khristopher Pineda <khristopher.pineda@aforecoppel.com>
2022-05-20 04:21:53 -0600

    inicio responsive to iphone 12 pro 📱

--- src/components/Inicio.tsx
+++ src/components/Inicio.tsx
@@ -1,23 +1,23 @@
 import { Link } from 'gatsby'
 import React from 'react'
 import ButtonCustom  from './ButtonCustom'
-import { SectionCustom } from './indexComponents'
+import { SectionInicioCustom, H1PresentCustom, H2Custom, H3Custom, PCustom } from './indexComponents'
 
 const Inicio = () => {
-    return <SectionCustom className='flex flex-col justify-center container px-[150px] py-[100px]'>
+    return <SectionInicioCustom className='flex flex-col justify-center container px-[150px] py-[100px]'>
         <div>
-            <h1 className='text-cyan-300 font-mono mb-5'>Hi, my name is</h1>
+            <H1PresentCustom className='text-cyan-300 font-mono mb-5'>Hi, my name is</H1PresentCustom>
         </div>
         <div>
-            <h2 className='text-slate-300 font-sans text-7xl capitalize font-bold tracking-tight mb-3'>khristopher pineda.</h2>
+            <H2Custom className='text-slate-300 font-sans text-7xl capitalize font-bold tracking-tight mb-3'>khristopher pineda.</H2Custom>
         </div>
         <div>
-            <h3 className='text-slate-400 font-sans text-7xl capitalize font-bold tracking-tight mb-5'>
+            <H3Custom className='text-slate-400 font-sans text-7xl capitalize font-bold tracking-tight mb-5'>
                 I build beautifull web sites.
-            </h3>
+            </H3Custom>
         </div>
         <div>
-            <p className='w-8/12 text-slate-400 font-sans text-lg mb-5'>I`m a Software Developer specializing in building (and occasionally designing) web sites.</p>
+            <PCustom className='w-8/12 text-slate-400 font-sans text-lg mb-5'>I`m a Software Developer specializing in building (and occasionally designing) web sites.</PCustom>
         </div>
         <div>
             <Link
@@ -26,7 +26,7 @@ const Inicio = () => {
                 <ButtonCustom text='Download Resumen' />
             </Link>
         </div>
-    </SectionCustom>
+    </SectionInicioCustom>
 }
 
 export default Inicio;
--- src/components/indexComponents.tsx
+++ src/components/indexComponents.tsx
@@ -1,8 +1,112 @@
 import styled from "styled-components";
 import TitleProps from "../interfaces/TitleProps";
 
+const hch = "4rem"
+export const fz1 = "2.25rem"
+export const ml1 = "1.6rem"
+export const mb1 = '.5rem'
+
+export const Contenido = styled.div`
+    @media (max-width: 640px) {
+        width: 100%;
+        text-aling: center;
+    }
+`
+
+export const SocialContainerCustom = styled.div`
+left: calc(50% - (32rem / 2));
+
+@media (max-width: 640px) {
+    position: relative;
+    bottom: 0;
+}
+`
+
+export const HeaderCustom = styled.header`
+@media (max-width: 640px) { 
+}
+`
+
+export const NavCustom = styled.header`
+    @media (max-width: 640px) { 
+        height: auto;
+    }
+    `
+
+export const Kcontainer = styled.div`
+    @media (max-width: 640px) { 
+        display: flex;
+        padding-left: ${ml1};
+    }
+    `
+
+export const Logo = styled.a`
+    @media (max-width: 640px) { 
+        padding-left: 0px;
+    }
+    `
+
 export const SectionCustom = styled.section`
     min-height: 100vh;
+    
+    @media (max-width: 640px) {
+        padding: 2.5rem ${ml1};
+    }
+    `
+
+export const ContactSectionCustom = styled(SectionCustom)`
+    @media (max-width: 640px) {
+    }
+`
+export const SectionInicioCustom = styled(SectionCustom)`
+    @media (max-width: 640px) {
+        margin-top: 1rem;
+    }
+`
+
+export const H1Custom = styled.h1`
+    @media (max-width: 640px) { 
+        width: 100%;
+        text-align: left;
+        margin-bottom: ${mb1};
+    }
+`
+
+export const H1PresentCustom = styled(H1Custom)`
+    @media (max-width: 640px) { 
+        font-size: 1rem
+    }
+    @media (max-width: 395px) {
+        font-size: 1.5rem;
+    }
+`
+export const H2Custom = styled.h2`
+    @media (max-width: 640px) { 
+        font-size: ${fz1};
+        margin-bottom: ${mb1};
+    }
+    @media (max-width: 395px) {
+        font-size: 3.75rem;
+    }
+`
+export const H3Custom = styled.h2`
+    @media (max-width: 640px) { 
+        font-size: ${fz1};
+        margin-bottom: ${mb1};
+    }
+    @media (max-width: 395px) {
+        font-size: 2.85rem;
+    }
+`
+export const PCustom = styled.p`
+    @media (max-width: 640px) { 
+        font-size: inherit;
+        line-height: inherit;
+    }
+    @media (max-width: 395px) {
+        font-size: 1.30rem;
+        width: 100%;
+    }
 `
 
 export const ButtonStyle = styled.button`
@@ -12,6 +116,11 @@ export const ButtonStyle = styled.button`
     user-select: none;
     border-color: var(--c3);
     color: var(--c3);
+
+    @media (max-width: 395px) {
+        font-size: 1rem;
+        width: 100%;
+    }
 `
 
 export const ListCustom = styled.li`
@@ -19,6 +128,21 @@ export const ListCustom = styled.li`
     -moz-user-select: none;
     -ms-user-select: none;
     user-select: none;
+`
+export const LineTitle = styled.div`
+    height: 1px;
+    border: none;
+
+    @media (max-width: 640px) { 
+        display: none
+    }
+`
+
+export const AboutCols = styled.div`
+    @media (max-width: 640px) { 
+        grid-column-end: 13;
+        grid-column-start: 1;
+    }
 `
 
 export const TitleCustom = styled.h2<TitleProps>`
@@ -37,6 +161,11 @@ export const TitleCustom = styled.h2<TitleProps>`
         height: 1px;
         width: 100%;
     }
+    
+    @media (max-width: 640px) { 
+        width: 100%;
+        text-align: center;
+    }
 `
 
 export const LinkCustom = styled.a`
@@ -52,7 +181,43 @@ export const LinkCustom = styled.a`
     cursor: pointer;
 `
 
+export const WorksContainer = styled.div`
+    @media (max-width: 640px) { 
+        grid-template-columns: 1fr;
+        width: inherit;
+
+        .card {
+            margin-left: 0px;
+            margin-right: 0px;
+        }
+
+    }
+`
+
 export const Social = styled.div`
     // background-color: var(--c2);
     margin: 0 1rem;
+`
+
+export const ContainerCards = styled.div`
+    @media (max-width: 640px) { 
+        grid-template-columns: 1fr;
+    }
+    & > .card {
+        flex-direction: column;
+    }
+`
+
+export const CardContainer = styled.div`
+@media (max-width: 640px) { 
+    flex-direction: column;
+    img {
+        height: inherit;
+        min-height: 100px;
+        max-height 150px;
+    }
+    .tarjeta {
+        margin: 0
+    }
+}
 `
