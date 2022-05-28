a76f29d1326ffcb74ef11c8f5adca1b93e8c1ebd
Khristopher Pineda <khristopher.pineda@aforecoppel.com>
2022-05-27 23:44:05 -0600

    problem padding in layout layout

--- src/components/utilities/SectionPage.tsx
+++ src/components/utilities/SectionPage.tsx
@@ -4,14 +4,17 @@ import SectionPageProps from "../../interfaces/SectionPageProps";
 const prueba = false
 
 const SectionPage = (props: SectionPageProps) => {
-    return <section id={props.id} className={`mx-auto max-w-[1536px] ${props.hScreem ?? 'md:h-screen'} flex flex-col box-border p-8
+    return <section id={props.id} className={`
+        mx-auto
+        max-w-[1536px] ${props.hScreem ?? 'md:h-screen'}
+        flex flex-col justify-center box-border px-8 py-4
         min-h-[480px] max-h-[${props.maxHeight ?? "760px"}]
         sm:h-auto
         md:max-h-['760px']
-        ${prueba ? 'sm:bg-black' : ''} sm:p-[4.5rem]
-        ${prueba ? 'md:bg-red-900' : ''} md:min-h-[580px] md:p-[7.450rem]
-        ${prueba ? 'lg:bg-amber-900' : ''} lg:p-[7rem] 
-        ${prueba ? 'xl:bg-emerald-900' : ''} xl:p-[9.375rem]
+        ${prueba ? 'sm:bg-black' : ''} sm:px-[4.5rem]
+        ${prueba ? 'md:bg-red-900' : ''} md:min-h-[580px] md:px-[7.450rem]
+        ${prueba ? 'lg:bg-amber-900' : ''} lg:px-[7rem] 
+        ${prueba ? 'xl:bg-emerald-900' : ''} xl:px-[9.375rem]
         ${prueba ? '2xl:bg-blue-900' : ''}
         ` + props.className}>{props.children}</section> 
 }
