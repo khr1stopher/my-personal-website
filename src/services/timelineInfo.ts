3929ee2c0a60a3421480ddec64362d117d9be929
Khristopher Pineda <khristopher.pineda@aforecoppel.com>
2022-06-03 14:31:01 -0600

    timeline

---
+++ src/services/timelineInfo.ts
@@ -0,0 +1,102 @@
+const workspace = [
+    {
+        date: 2021,
+        projectName: "coordinador validadores (re-ingenieering)",
+        madewith: ["typescript", "nodejs", "vue", "codeigneiter", "PostgreSQL"],
+        madeat: "ap/interfaces",
+        link: []
+    },
+    {
+        date: 2021,
+        projectName: "kompresor library",
+        madewith: ["nodejs", "brotli"],
+        madeat: "ap/interfaces",
+        link: [
+            "https://www.npmjs.com/package/kompresor",
+        ]
+    },
+    {
+        date: 2021,
+        projectName: 'Componente AFIB014',
+        madewith: ["spring boot", "vue"],
+        madeat: "ap/interfaces",
+        link: []
+    },
+    {
+        date: 2021,
+        projectName: "chevyplan project",
+        madewith: ["angular", ".net", "SQL server"],
+        madeat: "WCS",
+        link: ["https://www.chevyplan.com.co/"]
+    },
+    {
+        date: 2020,
+        projectName: 'Administrador de Pruebas',
+        madewith: ["c#","razor", "vstest"],
+        madeat: "WCS",
+        link: []
+    },
+    {
+        date: 2020,
+        projectName: "make use cases (200) to 'mis facturas' App",
+        madewith: ["c#", ".net", "vstest", "selenium"],
+        madeat: "WCS",
+        link: []
+    },
+    {
+        date: 2020,
+        projectName: "VISA landing page",
+        madewith: ["html", "css", "javascript", "php"],
+        madeat: "WCS",
+        link: []
+    },{
+        date: 2020,
+        projectName: "extract google tag manager data from excels and add to tags to project",
+        madewith: ["python", "pandas"],
+        madeat: "WCS",
+        link: []
+    },{
+        date: 2020,
+        projectName: "Kudiska",
+        madewith: ["angular", "nodejs", "express", "mongodb", "typescript"],
+        madeat: "Freelance",
+        link: []
+    },
+    {
+        date: 2020,
+        projectName: "re-engineering of .net to .net core (scotiabank)",
+        madewith: ["c#", ".net",".net core 3", "dapper", "sql server"],
+        madeat: "WCS",
+        link: []
+    },
+    {
+        date: 2020,
+        projectName: "landing page WCS",
+        madewith: ["html", "css", "javascript", "php", "jquery"],
+        madeat: "WCS",
+        link: ["https://wcslat.com/"]
+    },
+    {
+        date: 2020,
+        projectName: "chevyplan project",
+        madewith: ["angular", ".net", "SQL server"],
+        madeat: "WCS",
+        link: ["https://www.chevyplan.com.co/"]
+    },
+    {
+        date: 2019,
+        projectName: "final project of the high school",
+        madewith: ["html", "css", "javascript", "php", "mysql", "bootstrap", "jquery"],
+        madeat: "Manuel Palacios Fajardo",
+        link: []
+    },
+    {
+        date: 2018,
+        projectName: "landing page to high school",
+        madewith: ["html", "css", "javascript", "php", "wordpress"],
+        madeat: "Manuel Palacios Fajardo",
+        link: []
+    }
+];
+
+export default workspace;
