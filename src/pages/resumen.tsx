f03b5bad230e92c63edef65082107bead7e3404b
Khristopher Pineda <khristopher.pineda@aforecoppel.com>
2022-06-08 21:02:47 -0600

    💄 images update

--- src/pages/resumen.tsx
+++ src/pages/resumen.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import { resumen } from '../services/files'
 
+const styleResumen = {
+    Position: "absolute",
+    top: 0,
+    left: 0,
+    width: "100%",
+    height: "100%"
+}
+
+
 const Resumen = () => {
     return <div className="resumen">
-        <iframe src={resumen} style={{ height: '100vh', width: '100%' }}></iframe>
+        <iframe src={resumen} style={styleResumen}></iframe>
     </div>
 }
 
