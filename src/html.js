393c7f74e55e5b4ebe704f574729fc4123468d71
Khristopher Pineda <kkromans009@gmail.com>
2023-02-10 21:50:48 -0700

    icono

--- src/html.js
+++ src/html.js
@@ -12,6 +12,8 @@ export default function HTML(props) {
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
+        <link rel="icon" type="image/x-icon" href="/favicon.ico"></link>
+        <title>Khristopher Pineda</title>
         {props.headComponents}
       </head>
       <body {...props.bodyAttributes}>
