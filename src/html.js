1cc57fa9df324b7bd28f171c28b6be2054e1745e
khristopher_pineda <kkromans009@gmail.com>
2023-04-07 03:57:21 -0700

    💄 update icon

--- src/html.js
+++ src/html.js
@@ -12,7 +12,6 @@ export default function HTML(props) {
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
-        <link rel="icon" type="image/x-icon" href="/favicon.ico"></link>
         <title>Khristopher Pineda</title>
         {props.headComponents}
       </head>
