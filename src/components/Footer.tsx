0ec0c6f23cc8466740a0e894b7785f3fe9536e97
Khristopher Pineda <kkromans009@gmail.com>
2023-07-19 23:53:13 -0700

    Borde :(

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { SectionPage } from './indexComponents';
 
 const Footer = () => {
-    return <footer className='lg:border-t-2 border-slate-700  mt-5 bg- flex flex-wrap p-5 justify-center text-slate-300'>
+    return <footer className='border-t-2 border-slate-700  mt-5 bg- flex flex-wrap p-5 justify-center text-slate-300'>
             <h3>© 2023 Made with ❤️ by khristopher pineda</h3>
         </footer>
 }
