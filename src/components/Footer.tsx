3f3fee9184750646cc2e09b93fd05b8f7a351c7a
Khristoher Pineda <kkromans009@gmail.com>
2024-05-20 17:45:39 -0600

    add footer 💄

---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+const Footer = () => {
+    return <footer className="text-center py-4 border-t border-white text-sm tracking-wide text-white font-sans">
+        Made with 💪 by <a href="https://www.linkedin.com/in/khristopher-pineda/" target="_blank" className="underline">Khristopher Pineda</a>
+    </footer>
+}
+
+export default Footer;
