f64855098bb03b45efbf3ad47f6117503a2ca953
Khristopher <53666004+khr1stopher@users.noreply.github.com>
2026-06-04 19:34:09 -0700

    Develop (#10)
    * Feature/change npm for pnpm (#4)
    
    * change npm for pnpm
    
    * 💬 update email
    
    * packageManager
    
    * 🐛 fix pnpm update
    
    * Feature/update work section (#9)
    
    * changes (#8)
    
    * Feature/change npm for pnpm (#4) (#5) (#6)
    
    * Feature/change npm for pnpm (#4) (#5)
    
    * change npm for pnpm
    
    * 💬 update email
    
    * packageManager
    
    * 🐛 fix pnpm update
    
    * Update job to pnpm
    
    * Update pnpm action to use latest version
    
    Removed specific pnpm version to use latest.
    
    * max widht fix it (#7)
    
    * update work section

---
+++ src/components/service/work-list.ts
@@ -0,0 +1,11 @@
+import screenshot1 from '../../assets/Screenshot-1.jpg';
+import screenshot2 from '../../assets/Screenshot-2.jpg';
+import screenshot3 from '../../assets/Screenshot-3.jpg';
+
+import { angular, javascript, node, tailwindcss, vue } from '../../components/service/tech-img';
+
+export const WorkList = [
+    { id: 1, img: screenshot1, description: 'Tax Application CommunityBox', link: "https://www.communitybox.mx/", technologies: [node, angular] },
+    { id: 2, img: screenshot2, description: 'Landing page Chevyplan', link: "https://chevyplan.com.co/", technologies: [javascript] },
+    { id: 3, img: screenshot3, description: 'Coffeshop 1976', link: "https://github.com/khr1stopher/page_Coffe_shop", technologies: [vue, tailwindcss] },
+]
